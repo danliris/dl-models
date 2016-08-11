0d395ec6d34b69180ad40550c04c13005a698d95
SunlikeSiagian <sunlike.siagian@moonlay.com>
2016-08-11 17:08:24 +0700

    Resolve conflict

--- src/core/sparepart.js
+++ src/core/sparepart.js
@@ -1,20 +1,12 @@
 'use strict'
 
 var BaseModel = require("capital-models").BaseModel;
-<<<<<<< HEAD
-var UoM = require("./UoM-docs").UoM;
 
-module.exports = class Sparepart extends BaseModel {
-    constructor(source) {
-        super('sparepart', '1.0.0'); 
-=======
 var UoM = require("./UoM").UoM;
 
 module.exports = class Sparepart extends BaseModel {
     constructor(source) {
         super('sparepart', '1.0.0');
->>>>>>> 9b2204afe7dd0ee701ac5d27950175fefba20662
-
         this.code = '';
         this.name = '';
         this.description = '';
@@ -22,8 +14,4 @@ module.exports = class Sparepart extends BaseModel {
 
         this.copy(source);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9b2204afe7dd0ee701ac5d27950175fefba20662
